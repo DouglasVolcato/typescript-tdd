c51d1f2f73f83350080e585a6d35277f344c0ec5
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-22 04:33:11 -0300

    Added post route

--- index.ts
+++ index.ts
@@ -1,19 +1,26 @@
 import express, { Application, Request, Response, NextFunction } from "express";
 import cors from "cors";
 import { config } from "dotenv";
-
+import bodyParser from "body-parser";
 export const app: Application = express();
-const port = process.env.PORT || 3000;
-
 config();
+
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send({ message: "App is running." });
 });
 
-app.get("/auth", (req: Request, res: Response, next: NextFunction) => {
-  res.send({ message: "TESTE" });
+app.get("/dashboard", (req: Request, res: Response, next: NextFunction) => {
+  res.send({ message: "Dashboard." });
 });
 
+app.post("/post", (req: Request, res: Response, next: NextFunction) => {
+  const note: string = req.body.note;
+  res.send({ note: note });
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("http://localhost:" + port));
--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,21 @@
 import express from "express";
 import cors from "cors";
 import { config } from "dotenv";
+import bodyParser from "body-parser";
 export const app = express();
-const port = process.env.PORT || 3000;
 config();
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get("/", (req, res, next) => {
     res.send({ message: "App is running." });
 });
-app.get("/auth", (req, res, next) => {
-    res.send({ message: "TESTE" });
+app.get("/dashboard", (req, res, next) => {
+    res.send({ message: "Dashboard." });
 });
+app.post("/post", (req, res, next) => {
+    const note = req.body.note;
+    res.send({ note: note });
+});
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("http://localhost:" + port));
